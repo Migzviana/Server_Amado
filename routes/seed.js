6bfc6c30f7b0dddf98d37559bcb608cc5c20e9d4
migzviana <backupdosjogos2@gmail.com>
2026-03-19 15:25:15 -0300

    Commit Final 2

---
+++ routes/seed.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+
+router.get("/seed", async (req, res) => {
+  try {
+    const password = await bcrypt.hash("19122025", 10);
+
+    // cria casal
+    const couple = await prisma.couple.create({
+      data: {},
+    });
+
+    // cria usuários
+    const user1 = await prisma.user.create({
+      data: {
+        name: "Miguel",
+        email: "miguel@email.com",
+        password,
+        coupleId: couple.id,
+      },
+    });
+
+    const user2 = await prisma.user.create({
+      data: {
+        name: "Livia",
+        email: "liviaoliveira@email.com",
+        password,
+        coupleId: couple.id,
+      },
+    });
+
+    res.json({
+      couple,
+      user1,
+      user2,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro ao criar seed" });
+  }
+});
